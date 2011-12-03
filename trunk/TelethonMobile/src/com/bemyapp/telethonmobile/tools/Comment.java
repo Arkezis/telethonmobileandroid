e29e94950be52655a7df80b57558b58a7c67bc8f
clement.beslon@gmail.com <clement.beslon@gmail.com@cc29a1fe-5f9b-91e3-e47a-aed359a03dc7>
2011-12-03 16:40:01 +0000

    add comments list

---
+++ trunk/TelethonMobile/src/com/bemyapp/telethonmobile/tools/Comment.java
@@ -0,0 +1,23 @@
+package com.bemyapp.telethonmobile.tools;
+
+public class Comment {
+	private long note;
+	private String comment;
+
+	public void setNote(long note) {
+		this.note = note;
+	}
+
+	public long getNote() {
+		return note;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+}
