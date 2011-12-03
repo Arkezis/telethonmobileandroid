48d327356a603f07de72c1b1b6d048dcbe8ccf40
clement.beslon@gmail.com <clement.beslon@gmail.com@cc29a1fe-5f9b-91e3-e47a-aed359a03dc7>
2011-12-03 03:12:35 +0000

    create package constants

---
+++ trunk/TelethonMobile/src/com/bemyapp/telethonmobile/constants/CategoriesConstants.java
@@ -0,0 +1,15 @@
+package com.bemyapp.telethonmobile.constants;
+
+public class CategoriesConstants {
+
+	public final static int RESTAURANT = 1;
+	public final static int CAFE = 2;
+	public final static int MAGASIN = 3;
+	public final static int CINEMA = 4;
+	public final static int MUSEES = 5;
+	public final static int LOISIR = 6;
+	public final static int HOTEL = 7;
+	public final static int SANTE = 8;
+	public final static int TRANSPORT = 9;
+
+}
---
+++ trunk/TelethonMobile/src/com/bemyapp/telethonmobile/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.bemyapp.telethonmobile.constants;
+
+public class Constants {
+
+	public final static String LOG = "TELETHONMOBILE";
+
+}
