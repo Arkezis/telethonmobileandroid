39b48be9f0d681ef1c34957e06210205fe8d8827
tom.dubin <tom.dubin@cc29a1fe-5f9b-91e3-e47a-aed359a03dc7>
2011-12-03 02:59:40 +0000

    Constants + JSON

---
+++ trunk/TelethonMobile/src/com/bemyapp/telethonmobile/tools/JSONTools.java
@@ -0,0 +1,109 @@
+package com.bemyapp.telethonmobile.tools;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class JSONTools {
+	
+	public static JSONObject getJSONfromURL(String url){
+
+		//initialize
+		InputStream is = null;
+		String result = "";
+		JSONObject jArray = null;
+
+		//http post
+		try{
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet httpget = new HttpGet(url);
+			HttpResponse response = httpclient.execute(httpget);
+			HttpEntity entity = response.getEntity();
+			is = entity.getContent();
+
+			//convert response to string
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"),1024);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+			result=sb.toString();
+			Log.d(Constants.LOG,"getJSONfromURL  result "+ result);
+			//try parse the string to a JSON object
+	        	jArray = new JSONObject(result);
+		}catch(Exception e){
+			Log.d(Constants.LOG,"Error parsing data "+e.toString());
+			jArray=null;
+		}
+
+		return jArray;
+	}
+	
+	
+	public static JSONArray getJSONArrayFromInputStream(InputStream is){
+		String result = "";
+		JSONArray jArray = null;
+		try
+		{
+			//convert response to string
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"),1024);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+			result=sb.toString();
+			
+			Log.d(Constants.LOG,"getJSONfromURL  result "+ result);
+			//try parse the string to a JSON object
+	        	jArray = new JSONArray(result);
+		}catch(Exception e){
+			Log.d(Constants.LOG,"Error parsing data "+e.toString());
+			jArray=null;
+		}
+
+		return jArray;	
+	}
+	
+	public static JSONObject getJSONfromInputStream(InputStream is)
+	{
+		String result = "";
+		JSONObject jArray = null;
+		try
+		{
+			//convert response to string
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"),1024);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+			result=sb.toString();
+			Log.d(Constants.LOG,"getJSONfromURL  result "+ result);
+			//try parse the string to a JSON object
+
+	        	jArray = new JSONObject(result);
+		}catch(Exception e){
+			Log.e(Constants.LOG,"Error parsing data "+e.toString());
+			jArray=null;
+		}
+
+		return jArray;
+	}
+	
+	
+}
