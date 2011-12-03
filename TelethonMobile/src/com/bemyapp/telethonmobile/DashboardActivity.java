f31eb258fcaa839a22b230dc4585bfaf60eee4de
clement.beslon@gmail.com <clement.beslon@gmail.com@cc29a1fe-5f9b-91e3-e47a-aed359a03dc7>
2011-12-03 04:33:44 +0000

    fix problem on dashboard

--- TelethonMobile/src/com/bemyapp/telethonmobile/DashboardActivity.java
+++ TelethonMobile/src/com/bemyapp/telethonmobile/DashboardActivity.java
@@ -13,46 +13,49 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class DashboardActivity extends Activity implements TextToSpeech.OnInitListener, OnUtteranceCompletedListener{
+public class DashboardActivity extends Activity implements
+		TextToSpeech.OnInitListener, OnUtteranceCompletedListener {
 	/** Called when the activity is first created. */
-	
+
 	Button b1;
 	TextToSpeech myTTs;
-	HashMap<String, String> myHashAlarm = null;
+	HashMap<String, String> myHashAlarm = new HashMap<String, String>();
 
-	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dashboard);
-		
-		myTTs = new TextToSpeech(this,this);
 
-		b1 = (Button)findViewById(R.id.button1);
+		myTTs = new TextToSpeech(this, this);
+
+		b1 = (Button) findViewById(R.id.button1);
 		b1.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_STREAM,
+						String.valueOf(AudioManager.STREAM_ALARM));
+				myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID,
+						"End Message");
 				myTTs.setOnUtteranceCompletedListener(DashboardActivity.this);
-				myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_STREAM, String.valueOf(AudioManager.STREAM_ALARM));
-				myTTs.speak("Restaurant", TextToSpeech.QUEUE_FLUSH	, myHashAlarm);
-				myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID,"End Message");
+				myTTs.speak("Restaurant", TextToSpeech.QUEUE_FLUSH, myHashAlarm);
 			}
 		});
 	}
 
+	@Override
 	public void onUtteranceCompleted(String uttId) {
-	    if (uttId == "End Message") {
-	    	Intent i = new Intent(DashboardActivity.this,MapViewActivity.class);
-	    	startActivity(i);
-	    	finish();
-	    } 
+		if (uttId.equals("End Message")) {
+			Intent i = new Intent(DashboardActivity.this, MapViewActivity.class);
+			startActivity(i);
+			finish();
+		}
 	}
-	
+
 	@Override
 	public void onInit(int status) {
 		// TODO Auto-generated method stub
-		Locale loc = new Locale("fr", "","");
-		if(myTTs.isLanguageAvailable(loc) >= TextToSpeech.LANG_AVAILABLE){
+		Locale loc = new Locale("fr", "", "");
+		if (myTTs.isLanguageAvailable(loc) >= TextToSpeech.LANG_AVAILABLE) {
 			myTTs.setLanguage(loc);
 		}
 	}
