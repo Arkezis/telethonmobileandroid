9a5367450c974f47ea27da2e1ec52ca269d64b4e
clement.beslon@gmail.com <clement.beslon@gmail.com@cc29a1fe-5f9b-91e3-e47a-aed359a03dc7>
2011-12-03 02:43:34 +0000

    add modif on map

--- trunk/TelethonMobile/src/com/bemyapp/telethonmobile/DashboardActivity.java
+++ trunk/TelethonMobile/src/com/bemyapp/telethonmobile/DashboardActivity.java
@@ -1,6 +1,7 @@
 package com.bemyapp.telethonmobile;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class DashboardActivity extends Activity {
@@ -9,6 +10,7 @@ public class DashboardActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dashboard);
+		startActivity(new Intent(this, MapViewActivity.class));
 	}
 
 }
